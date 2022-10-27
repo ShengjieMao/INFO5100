3d0cd6e89898407e9a23db22af4e07586e12c181
FloraM <56333697+ShengjieMao@users.noreply.github.com>
2022-10-27 00:08:50 -0700

    Update Main.java

--- Assignment6/Main.java
+++ Assignment6/Main.java
@@ -1,5 +1,11 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame("weightedCalc");
+        frame.setContentPane(new weightedCalc().weightedScore);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
