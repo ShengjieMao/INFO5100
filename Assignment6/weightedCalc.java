c0af97403c717c567f8aa6a3a980155c9c09b378
FloraM <56333697+ShengjieMao@users.noreply.github.com>
2022-10-27 00:09:42 -0700

    Update weightedCalc.java

--- Assignment6/weightedCalc.java
+++ Assignment6/weightedCalc.java
@@ -1,9 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class weightedCalc {
-    private JPanel weightedScore;
+
+public class weightedCalc extends JFrame{
+    // initialize the variables
+    public JPanel weightedScore;
     private JTextField TotalPoints;
     private JTextField Percentage;
     private JTextField Earned;
@@ -15,6 +18,7 @@ public class weightedCalc {
     private JLabel result;
 
 
+    // process the button handler and result action
     public weightedCalc() {
         Calculate.addActionListener(new ActionListener() {
             @Override
@@ -29,11 +33,53 @@ public class weightedCalc {
         });
     }
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("weightedCalc");
-        frame.setContentPane(new weightedCalc().weightedScore);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+    {
+    // GUI initializer
+        $$$setupUI$$$();
     }
+
+
+    // uses the GUI and set up the initial values and labels
+    public void $$$setupUI$$$() {
+        GridLayout gb = new GridLayout(0, 1);
+        weightedScore = new JPanel();
+        weightedScore.setLayout(gb);
+        total = new JLabel();
+        total.setText("Total Assignment Points");
+        weightedScore.add(total);
+        TotalPoints = new JTextField(20);
+        TotalPoints.setSize(120,30);
+        weightedScore.add(TotalPoints);
+        perc = new JLabel();
+        perc.setText("Percentage of Class");
+        weightedScore.add(perc);
+        Percentage = new JTextField(20);
+        Percentage.setSize(120,30);
+        weightedScore.add(Percentage);
+        raw = new JLabel();
+        raw.setText("Earned Points");
+        weightedScore.add(raw);
+        Earned = new JTextField(20);
+        Earned.setSize(120,30);
+        weightedScore.add(Earned);
+        Calculate = new JButton();
+        Calculate.setText("Click to calculate score");
+        weightedScore.add(Calculate);
+        result = new JLabel();
+        result.setText("Weighted score");
+        weightedScore.add(result);
+        Weighted = new JTextField(20);
+        Weighted.setSize(120,30);
+        Weighted.setText("");
+        weightedScore.add(Weighted);
+    }
+
+
+    // running codes
+    public JComponent $$$getRootComponent$$$() {
+        return weightedScore;
+    }
+
+
 }
+
